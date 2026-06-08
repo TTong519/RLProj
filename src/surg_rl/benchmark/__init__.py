af1667c5394f554a299f9c0bf74ecb4d788e95be
Tyler <tylertong519@gmail.com>
2026-06-08 11:01:28 -0700

    feat(23-02): export ExperimentRunner and metrics from benchmark package

--- src/surg_rl/benchmark/__init__.py
+++ src/surg_rl/benchmark/__init__.py
@@ -7,5 +7,16 @@ Install: pip install surg-rl[benchmark]
 from surg_rl.utils.lazy_imports import LazyImport
 
 MATPLOTLIB = LazyImport("matplotlib", "benchmark")
+SEABORN = LazyImport("seaborn", "benchmark")
+PANDAS = LazyImport("pandas", "benchmark")
+RLIABLE = LazyImport("rliable", "benchmark")
 
-__all__ = ["MATPLOTLIB"]
+from surg_rl.benchmark.experiment_config import ExperimentConfig
+from surg_rl.benchmark.experiment_runner import ExperimentRunner
+from surg_rl.benchmark.metrics import MetricCollectorCallback, Aggregator
+
+__all__ = [
+    "MATPLOTLIB", "SEABORN", "PANDAS", "RLIABLE",
+    "ExperimentConfig", "ExperimentRunner",
+    "MetricCollectorCallback", "Aggregator",
+]
